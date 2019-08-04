9f7fe188f4078b130428db7b9327dfdf2e7cfc70
ferdn4ndo <const.fernando@gmail.com>
2019-08-04 16:10:04 -0300

    Test

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.set('port', (process.env.PORT || 5000));
+
+app.listen(app.get('port'), function () {
+  console.log('Example app listening on port 3000!');
+});
