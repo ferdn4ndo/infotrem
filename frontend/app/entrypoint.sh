9181d25ad74a4739fe3a72449a6f092caf48a176
Fernando Silva <fernando.silva@crossknowledge.com>
2020-06-21 18:41:24 -0300

    Fixed port

--- frontend/app/entrypoint.sh
+++ frontend/app/entrypoint.sh
@@ -3,8 +3,8 @@
 if [ "${ENV_MODE}" = "prod" ]; then
   echo "Starting in production mode..."
   yarn run build
-  serve --host 0.0.0.0 -l 9903 -s dist
+  serve --host 0.0.0.0 -l 9906 -s dist
 else
   echo "Starting in development mode..."
-  cross-env BABEL_ENV=dev vue-cli-service serve --port=9903 --disable-host-check
+  cross-env BABEL_ENV=dev vue-cli-service serve --port=9906 --disable-host-check
 fi
