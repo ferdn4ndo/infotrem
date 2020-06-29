9b523d38d31c0cd65cca8bbf863c0fd421f12c22
Fernando Silva <fernando.silva@crossknowledge.com>
2020-06-28 23:33:46 -0300

    Deleted extra view

--- backend/infotrem/views/railroad_paint_scheme_view.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import viewsets
-from rest_framework.generics import get_object_or_404
-from rest_framework.response import Response
-
-from infotrem.services.policy import IsLoggedIn, IsModeratorOrReadOnly
-
