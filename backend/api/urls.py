c6e82ec46ec045d64613c918e86311dd72a7a562
Fernando Constantino <const.fernando@gmail.com>
2022-09-23 19:46:43 -0300

    Upgraded django and other packages

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,5 @@
-from django.conf.urls import url
 from django.contrib import admin
-from django.urls import include
+from django.urls import include, re_path
 
 from api.forms.user_forms import UserAdminForm
 
@@ -27,22 +26,22 @@ from core.models.user.user_model import User
 admin.site.register(User, UserAdminForm)
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^albums/', include(album_router.urlpatterns)),
-    url(r'^cronjob/', include(cronjob_router.urlpatterns)),
-    url(r'^contact', include(contact_router.urlpatterns)),
-    url(r'^comments/', include(comment_router.urlpatterns)),
-    url(r'^companies/', include(company_router.urlpatterns)),
-    url(r'^docs/', include(doc_router.urlpatterns)),
-    url(r'^email-validation/', include(email_validation_router.urlpatterns)),
-    url(r'^health', include(health_router.urlpatterns)),
-    url(r'^login', include(login_router.urlpatterns)),
-    url(r'^information/', include(information_router.urlpatterns)),
-    url(r'^locations/', include(location_router.urlpatterns)),
-    url(r'^manufacturers/', include(manufacturer_router.urlpatterns)),
-    url(r'^me', include(me_router.urlpatterns)),
-    url(r'^register', include(register_router.urlpatterns)),
-    url(r'^states/', include(state_router.urlpatterns)),
-    url(r'^users/', include(user_router.urlpatterns)),
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    re_path(r'^albums/', include(album_router.urlpatterns)),
+    re_path(r'^cronjob/', include(cronjob_router.urlpatterns)),
+    re_path(r'^contact', include(contact_router.urlpatterns)),
+    re_path(r'^comments/', include(comment_router.urlpatterns)),
+    re_path(r'^companies/', include(company_router.urlpatterns)),
+    re_path(r'^docs/', include(doc_router.urlpatterns)),
+    re_path(r'^email-validation/', include(email_validation_router.urlpatterns)),
+    re_path(r'^health', include(health_router.urlpatterns)),
+    re_path(r'^login', include(login_router.urlpatterns)),
+    re_path(r'^information/', include(information_router.urlpatterns)),
+    re_path(r'^locations/', include(location_router.urlpatterns)),
+    re_path(r'^manufacturers/', include(manufacturer_router.urlpatterns)),
+    re_path(r'^me', include(me_router.urlpatterns)),
+    re_path(r'^register', include(register_router.urlpatterns)),
+    re_path(r'^states/', include(state_router.urlpatterns)),
+    re_path(r'^users/', include(user_router.urlpatterns)),
 ]
